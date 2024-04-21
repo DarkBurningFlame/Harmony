6dbfe9f85d2b175d1e658096141162424b673d37
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2024-04-21 11:01:23 -0400

    Update Server.Fields.cs

--- Flames/Server/Server.Fields.cs
+++ Flames/Server/Server.Fields.cs
@@ -39,7 +39,7 @@ namespace Flames
         public static PlayerMetaList RankInfo = new PlayerMetaList("text/rankinfo.txt");
         public static PlayerMetaList Notes = new PlayerMetaList("text/notes.txt");
         /// <summary> *** DO NOT USE THIS! *** Use VersionString, as this field is a constant and is inlined if used. </summary>
-        public const string InternalVersion = "0.0.5.2";
+        public const string InternalVersion = "0.0.6.1";
         public static string Version { get { return InternalVersion; } }
         public static string SoftwareName = "&dH&1a&er&cm&6o&6n&0y &4Dev";
         static string fullName;
